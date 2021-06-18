217c14212707d8a79dcc5f97d8ded8c068c9a95a
SebagadisK <sebexk3@gmail.com>
2021-06-18 12:25:42 -0700

    Create 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	printf("%i", d);
+	putchar('\n');
+
+	return (0);
+}
