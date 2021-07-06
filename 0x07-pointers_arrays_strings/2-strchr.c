81798e096815e1c2735fa399261690780e9d31ed
SebagadisK <sebexk3@gmail.com>
2021-07-06 01:44:58 -0700

    update 2-strchr.c for correct output

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
-#include <stddef.h>
+
 /**
  * _strchr - locates a character in a string
- * @s: the string
- * @c: the character
+ * @s: string to check
+ * @c: character to check for
  *
- * Return: a pointer to a character aka the character found
+ * Return: pointer to spot in s with c or null
  */
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0' ; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		if (s[i] == c)
-		return (&s[i]);
+		if (*(s + i) == c)
+			return (s + i);
 	}
+	if (*(s + i) == c)
+		return (s + i);
 	return (0);
 }
