122e6d994f0d3ebf7c3ac072655941708e67ef2a
SebagadisK <sebexk3@gmail.com>
2021-08-05 14:57:00 -0700

    Add 1-print_binary.c and update holberton.h, main.h

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif /* _HOLBERTON_H_ */
